078de2db0de2d7f9bcad194430952353dc66b6ab
Madhav-Garg <sidharthgarg29082004@gmail.com>
2025-07-31 11:38:10 +0530

    Added personal portfolio

---
+++ portfolio/script.js
@@ -0,0 +1,21 @@
+//Home-page
+document.getElementById('hireBtn').addEventListener('click', function() {
+    window.location.href = 'contact.html'; // Change this to the URL of your contact page
+});
+
+//About-me page
+document.getElementById('hireBt').addEventListener('click', function() {
+    window.location.href = 'aboutme.html'; // Change this to the URL of your contact page
+});
+//services-page
+document.getElementById('hire').addEventListener('click', function() {
+    window.location.href = 'services.html'; // Change this to the URL of your contact page
+});
+//services-page
+document.getElementById('hir').addEventListener('click', function() {
+    window.location.href = 'services.html'; // Change this to the URL of your contact page
+});
+//services-page
+document.getElementById('hi').addEventListener('click', function() {
+    window.location.href = 'services.html'; // Change this to the URL of your contact page
+});
